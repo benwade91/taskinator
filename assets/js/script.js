990684f687131af88b6ca02eab2cf59cd8fd8f68
benwade <70988204+benn925@users.noreply.github.com>
2020-12-15 14:03:37 -0800

    added customization to submit-form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     event.preventDefault();
-    
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "Hello";
     listItemEl.className = "task-item";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 formEl.addEventListener("submit", createTaskHandler);
 
